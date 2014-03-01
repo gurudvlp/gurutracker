5a24e30a2df289df0d21f5a7500130337792364e
Brian Murphy <gurudvlp@gmail.com>
2014-03-01 13:55:12 -0500

    Continued writing the loading class for the .gt file format.  This commit probably breaks the new loading method because it only reads sample headers and sound data.  It does not read the saved data for wave generators and wave machines.

--- gurumod/Tracks/Serializers/LoadGT-0001.cs
+++ gurumod/Tracks/Serializers/LoadGT-0001.cs
@@ -160,8 +160,163 @@ namespace gurumod.Serializers
 				}
 			}
 
+			thetrack.Patterns = inpatterns;
 
+
+
+			thetrack.Samples = GrabSamples(reader, nosamps);
 			return thetrack;
+		}
+
+		public static Sample[] GrabSamples(BinaryReader reader, int numsamples)
+		{
+			//	Now for sample data.  It gets a little more tricky.
+			//		iiiiiyyyybbbbbbbbbbbbbbbbbbbcppppppppppssssssssssgmddddddddddddddddddd
+			//			i: 5 digit sample id
+			//			y: 4 digit year
+			//			b: 19 digit bit rate
+			//			c: 1 digit number of channels
+			//			p: 10 digit bits per sample
+			//			s: 10 digit sample rate
+			//			g: 1 digit for using a wave generator (0 or 1)
+			//			m: 1 digit for using a wave machine (0 or 1)
+			//			d: 19 digit length of sound data
+			//		n<chr 0>a<chr 0>f<chr 0>
+			//			n: name of sample
+			//			a: artist
+			//			f: filename of sample
+			//		sounddata as bytes
+			//
+			//	If the wave generator flag is 1, then:
+			//		tfffffssssssllll
+			//			t: sound type
+			//			f: 5 digit frequency
+			//			s: 6 digit sample rate
+			//			l: 4 digit length in seconds
+			//
+			//	If the wave machine flag is 1, then:
+			//		ssssssfffffgggppp
+			//			s: 6 digit sample rate
+			//			f: 5 digit frequency
+			//			g: 3 digit number of generators
+			//			p: 3 digit number of processors
+			//	For each generator:
+			//		tessssssfffffaaaa
+			//			t: 1 digit generator type
+			//			e: 1 digit enabled (0 or 1)
+			//			s: 6 digit sample rate
+			//			f: 5 digit frequency
+			//			a: 4 digit amplitude (x.xx)
+			//	For generator type 0 (oscillator)
+			//		w
+			//			w: 1 digit wave type
+			//	For generator type 1 (wave player)
+			//		cssssssbbbbbbbbbblllllllllllllllllllf<chr 0>
+			//			c: 1 digit channel count
+			//			s: 6 digit sample rate
+			//			b: 10 digit bit rate
+			//			l: 19 digit length of audio data
+			//			f: filename
+			//		list of 2-byte shorts of audio data
+			//	For each processor:
+			//		nnniiittt
+			//			n: 3 digit processor id
+			//			i: 3 digit number of inputs
+			//			t: 3 digit processor type
+			//	For each input of the processor:
+			//		iiitttaaaa
+			//			i: 3 digit source id
+			//			t: 3 digit source type
+			//			a: 4 digit amplitude (x.xx)
+			//	For mixers
+			//		m
+			//			m: 1 digit combine method
+			//	For envelopes:
+			//		aaaaaaaaaabbbbddddddddddeeeessssssssssrrrrrrrrrr
+			//			a: 10 digit attack
+			//			b: 4 digit attack amplitude (x.xx)
+			//			d: 10 digit decay
+			//			e: 4 digit decay amplitude (x.xx)
+			//			s: 10 digit sustain
+			//			r: 10 digit release
+			//	For gates
+			//		llllllllllhhhhhhhhhh
+			//			l: 10 digit MinGateManual
+			//			h: 10 digit MaxGateManual
+			//	For reverb
+			//		wwwwwwwwwwdddddddddd
+			//			w: 10 digit delay
+			//			d: 10 digit decay
+
+			Sample[] insamps = new Sample[numsamples];
+
+			for(int esamp = 0; esamp < numsamples; esamp++)
+			{
+				int sampid = -1;
+				int year = 0;
+				long bitrate = 44100;
+				int nochans = 1;
+				long bitspersample = 0;
+				long samplerate = 0;
+				int wavegen = 0;
+				int wavemach = 0;
+				long sounddatalen = 0;
+				string sampname = "";
+				string artist = "";
+				string sampfilename = "";
+
+				if(!GrabInt(reader, out sampid, 5)
+				   || !GrabInt(reader, out year, 4)
+				   || !GrabLong(reader, out bitrate, 19)
+				   || !GrabInt(reader, out nochans, 1)
+				   || !GrabLong(reader, out bitspersample, 10)
+				   || !GrabLong(reader, out samplerate, 10)
+				   || !GrabInt(reader, out wavegen, 1)
+				   || !GrabInt(reader, out wavemach, 1)
+				   || !GrabLong(reader, out sounddatalen, 19))
+				{
+					Console.WriteLine("Sample header {0} is corrupt.", esamp);
+					return null;
+				}
+
+				insamps[sampid] = new Sample();
+				insamps[sampid].ID = sampid;
+				insamps[sampid].Year = year;
+				insamps[sampid].BitRate = bitrate;
+				insamps[sampid].channels = nochans;
+				insamps[sampid].bits_per_sample = bitspersample;
+				insamps[sampid].sample_rate = samplerate;
+
+				if(wavegen == 1) { insamps[sampid].UseWaveGenerator = true; }
+				if(wavemach == 1) { insamps[sampid].UseWaveMachine = true; }
+
+				sampname = GrabString(reader);
+				artist = GrabString(reader);
+				sampfilename = GrabString(reader);
+
+				insamps[sampid].Name = sampname;
+				insamps[sampid].Artist = artist;
+				insamps[sampid].Filename = sampfilename;
+
+				if(sounddatalen > 0)
+				{
+					insamps[sampid].SoundData = new byte[sounddatalen];
+					insamps[sampid].SoundData = reader.ReadBytes (sounddatalen);
+				}
+
+				if(wavegen == 1)
+				{
+
+				}
+
+				if(wavemach == 1)
+				{
+
+				}
+
+			}
+
+			return insamps;
 		}
 
 		public static bool GrabInt(BinaryReader reader, out int intval, int length)
