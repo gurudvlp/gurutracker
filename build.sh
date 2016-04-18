f1e27c077c220d817329180750064a609ffb8cbe
Brian Murphy <gurudvlp@gmail.com>
2016-04-18 09:28:06 -0400

    Moved location of raw resources.

--- build.sh
+++ build.sh
@@ -7,13 +7,15 @@
 ########################################################################
 
 mkdir -p gurumod/Resources/bin
-tar -cvf gurumod/Resources/Samples.tar samples
-tar -cvf gurumod/Resources/Interfaces.tar Interfaces
-tar -cvf gurumod/Resources/Tracks.tar Tracks
+cd Resources
+tar -cvf ../gurumod/Resources/Samples.tar samples
+tar -cvf ../gurumod/Resources/Interfaces.tar Interfaces
+tar -cvf ../gurumod/Resources/Tracks.tar Tracks
 
+cd ..
 rm gurumod/Resources/bin/* -rf
 cp bin/Debug gurumod/Resources/bin
 
 tar -cvf gurumod/Resources/Bin.tar bin
 
-mdtool build gurutracker.sln > buildlog.txt
+#mdtool build gurutracker.sln > buildlog.txt
