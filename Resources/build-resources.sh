07e0d94c84c6c4fdf57b7d32c173d731b7bbb7eb
Brian Murphy <gurudvlp@gmail.com>
2016-04-18 09:44:21 -0400

    Set up so that the libs are also bundled as a resource.

--- Resources/build-resources.sh
+++ Resources/build-resources.sh
@@ -4,14 +4,17 @@
 #	This script builds archives of the raw resources.
 #
 ########################################################################
+
 mkdir -p gurumod/Resources/bin
 cd Resources
 tar -cvf ../gurumod/Resources/Samples.tar samples
 tar -cvf ../gurumod/Resources/Interfaces.tar Interfaces
 tar -cvf ../gurumod/Resources/Tracks.tar Tracks
+tar -cvf ../gurumod/Resources/Libraries.tar Libraries
 
 cd ..
 rm gurumod/Resources/bin/* -rf
+cp Resources/Libraries/* bin/Debug
 cp bin/Debug gurumod/Resources/bin
 
 tar -cvf gurumod/Resources/Bin.tar bin
